ef04d7c17c6447190b858bb49a797a16bd799d28
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2023-08-01 13:05:27 +0200

    initial commit expand from creTab

--- creTabV2.sql
+++ creTabV2.sql
@@ -1,16 +1,28 @@
-create schema if not exists ndocstore;
+create schema if not exists docstore;
+DO $$ BEGIN
 create type docstore.imgtyp as enum ('jpg','png','bmp','gif','tiff','eps','raw');
+Exception
+	when duplicate_object then null;
+END $$;
+
 create table if not exists docstore.buckets (
-uuid uuid,
+buckId uuid,
+id serial,
 name varchar(20),
 owner varchar(20)
 );
 create table if not exists docstore.objects (
-id uuid,
-bucket uuid,
+objId uuid,
+id serial,
 name varchar(20),
 createdOn timestamp,
 uploadedBy varchar(25),
 mime varchar(30),
 supp json
+);
+create table if not exists docstore.users (
+userId serial,
+email varchar(25) unique,
+createdOn timestamp,
+priv varchar(25) []
 );
